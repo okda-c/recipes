540dc990853a03818357da3de3e19ce1ede963f2
Eva <tlox_xaxaxa@mail.ru>
2025-02-27 20:10:05 +0300

    almost done

--- totot/urls.py
+++ totot/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from core.views import index
+from core.views import pie 
+from core.views import fried
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='index.html'))
+    path('',index),
+    path('pie',pie),
+    path('fried',fried)
 ]
